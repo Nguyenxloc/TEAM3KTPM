51bea772e1ae5c5ef4e466143f2444facf8288ee
84374 <84374@DESKTOP-KJ1SU2P>
2023-05-20 09:29:05 +0700

    Bo sung mo hinh DAO, ae lam theo DAO nhe

--- src/main/java/repository/DAOlichHoc.java
+++ src/main/java/repository/DAOlichHoc.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package view;
+package repository;
 
 /**
  *
  * @author 84374
  */
-public class appFrame {
+public class DAOlichHoc {
     
 }
